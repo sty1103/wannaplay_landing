de7c16b175f4fae873c0eae3789c2438c15a2e7a
Hyeonjun <sty110357@gmail.com>
2022-03-31 03:20:17 +0000

    [UPDATE] 2nd publishing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.set('port', process.env.PORT || 8080);
@@ -11,6 +12,34 @@ app.use('/fonts', express.static(__dirname + '/fonts'));
 
 app.get('/', (req, res) => {
   res.sendFile( path.join( __dirname, '/index.html') );
+});
+
+app.get('/video/:num', (req, res) => {
+  const dest = __dirname + '/videos/identity_' + req.params.num + '.mp4';
+  const stream = fs.createReadStream(dest); // 스트림 객체
+
+  const total = fs.statSync(dest).size; // 파일 총 크기
+  const range = req.headers.range; // 요청받은 영상 재생 범위
+  const positions = range.replace(/bytes=/, "").split("-");
+  const start = parseInt(positions[0]); // 영상 시작 지점
+  const end = positions[1] ? parseInt(positions[1]) : total - 1; // 영상 끝 지점
+  const chunksize = (end -start) + 1; // 영상을 얼마만큼 잘라서 보내줄지
+
+  stream.on('open', () => {
+    res.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Accept-Range": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    });
+    stream.pipe(res);
+  });
+
+  stream.on('error', (err) => {
+    console.log(err);
+    res.end(err);
+  });
+
 });
 
 app.listen(app.get('port'), () => {
